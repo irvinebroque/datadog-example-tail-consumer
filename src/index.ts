48bbd229ba7055b4679ab04805b47cbc800b6b02
Brendan Irvine-Broque <birvine-broque@cloudflare.com>
2025-05-12 12:58:13 -0700

    Basics

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,33 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { client, v2 } from '@datadog/datadog-api-client';
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async tail(events: TraceItem[], env: Env) {
+		const configuration = client.createConfiguration({
+			authMethods: {
+				apiKeyAuth: env.DATADOG_API_KEY,
+				appKeyAuth: env.DATADOG_APP_KEY,
+			},
+		});
+		const apiInstance = new v2.LogsApi(configuration);
+
+		const params: v2.LogsApiSubmitLogRequest = {
+			body: [
+				{
+					ddsource: 'cloudflare-worker',
+					ddtags: 'some-tag',
+					hostname: 'some-hostname',
+					message: '2019-11-19T14:37:58,995 INFO [process.name][20081] Hello World',
+					service: 'my-service',
+				},
+			],
+			contentEncoding: 'gzip',
+		};
+
+		await apiInstance
+			.submitLog(params)
+			.then((data: any) => {
+				console.log('API called successfully. Returned data: ' + JSON.stringify(data));
+			})
+			.catch((error: any) => console.error(error));
 	},
 } satisfies ExportedHandler<Env>;
